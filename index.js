3d03f5fc84ab91d0a14b235417f41c35e76dc8e1
HOME\kim.nakhyun <kim_nakhyun@radcns.com>
2023-12-12 16:45:26 +0900

    lodash import

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import circle from "./circle.js";
+import lodash from "lodash";
 
-console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
+const uniqueArr = lodash.uniqBy(arr);
+
+console.log(uniqueArr);
