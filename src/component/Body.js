040012fa69c948d9090d356bd6aef30db899d290
HOME\kim.nakhyun <kim_nakhyun@radcns.com>
2023-12-15 16:45:50 +0900

    Changes

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,28 +1,27 @@
-import {useState} from "react";
-
-function Viewer({}) {
-    console.log("viewer component update!");
-    return <div>Viewer</div>
-}
+import {useRef,useState} from "react";
 
 function Body() {
-    const [number, setNumber] = useState(0);
-    const onIncrease = () => {
-        setNumber(number + 1);
+    const [text, setText] = useState("");
+    const textRef = useRef();
+    const handleOnchange = (e) => {
+        setText(e.target.value);
     };
-    const onDecrease = () => {
-        setNumber(number - 1)
-    };
-    return (
-       <div>
-           <h2>{number}</h2>
-           <Viewer />
 
-           <div>
-               <button onClick={onDecrease}>-</button>
-               <button onClick={onIncrease}>+</button>
-           </div>
-       </div>
+    const handleOnclick = function () {
+        if (text.length  < 5) {
+            textRef.current.focus();
+        } else {
+            alert(text);
+            setText("");
+
+        }
+    };
+
+    return (
+        <div>
+            <input ref={textRef} value={text} onChange={handleOnchange}/>
+            <button onClick={handleOnclick}>작성완료</button>
+        </div>
     );
 }
 
