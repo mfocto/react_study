04c7374ed0d076f492d41dc4d1237ea27c49979b
mfocto <skrgus0140@naver.com>
2023-12-17 23:47:16 +0900

    23-12-17

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import Header from "./component/Header"
-import Body from "./component/Body"
-import Footer from "./component/Footer"
-
+import {useEffect,useState} from "react";
+import Viewer from './component/Viewer'
+import Controller from './component/Controller'
 
 function App() {
-  const name = "123";
-  return (
-    <div className="App">
-      <Header />
-      <Body name={name}/>
-      <Footer />
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const handleSetCount = (value) => {
+        setCount(count + value);
+    }
+    useEffect(() => {
+        console.log("count update:", count);
+    }, [count]);
+    return (
+        <div className="App">
+            <h1>Simple Counter</h1>
+            <section>
+                <Viewer count={count}/>
+            </section>
+            <section>
+                <Controller handleSetCount={handleSetCount}/>
+            </section>
+        </div>
+    );
 }
 
 export default App;
