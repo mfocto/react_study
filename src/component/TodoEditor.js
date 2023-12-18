c2aa4a289c4a143ec3e6117e8a6dea63bb3e6e81
mfocto <skrgus0140@naver.com>
2023-12-18 23:47:44 +0900

    12-18

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,17 +1,34 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 import '../css/TodoEditor.css'
 
 const TodoEditor = ({onCreate}) => {
     const [content, setContent] = useState("");
+    const inputRef = useRef();
     const onChangeContent = (e) => {
       setContent(e.target.value);
     };
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    const onSubmit = () => {
+        if (!content){
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
     return (
         <div className="TodoEditor">
             <h4>✒새로운 todo 작성</h4>
             <div className="editorWrapper">
-                <input value={content} onChange={onChangeContent} placeholder="new Todo....."/>
-                <button>추가</button>
+                <input ref={inputRef} value={content} onChange={onChangeContent} onKeyDown={onKeyDown} placeholder="new Todo....."/>
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     );
